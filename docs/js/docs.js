c8d1cb4ce2bd8dea711d726b7cec05db976d2831
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2018-08-23 17:59:25 -0300

    Visualizing first page

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,6 +1,6 @@
-var analysisArray = ['first-glance', 'station-map', 'time-of-day-trends', 'seasonal-trends']
-var analysisDict = {'A-first-glance-at-the-Divvy-data-1': 'first-glance', 'Divvy-usage-over-the-year-1': 'seasonal-trends', 'A-map-of-the-Divvy-stations-in-Chicago-1': 'station-map', 'Divvy-usage-by-time-of-day-1': 'time-of-day-trends'}
-var analysisArrayMap = {'first-glance': 'A first glance at the Divvy data', 'seasonal-trends': 'Divvy usage over the year', 'station-map': 'A map of the Divvy stat ... s in Chicago', 'time-of-day-trends': 'Divvy usage by time-of-day'}
+var analysisArray = ['sgd-and-variations', 'station-map', 'time-of-day-trends', 'seasonal-trends']
+var analysisDict = {'Simple-Neural-Networks-1': 'Simple Neural Netwoks', 'A-first-glance-at-the-Divvy-data-1': 'first-glance', 'Divvy-usage-over-the-year-1': 'seasonal-trends', 'SGD-and-variations-1': 'sgd-and-variations', 'A-map-of-the-Divvy-stations-in-Chicago-1': 'station-map', 'Divvy-usage-by-time-of-day-1': 'time-of-day-trends'}
+var analysisArrayMap = {'Simple Neural Netwoks': 'Simple Neural Networks', 'first-glance': 'A first glance at the Divvy data', 'seasonal-trends': 'Divvy usage over the year', 'sgd-and-variations': 'SGD and variations', 'station-map': 'A map of the Divvy stat ... s in Chicago', 'time-of-day-trends': 'Divvy usage by time-of-day'}
 var setupArray = []
 var setupDict = {}
 var setupArrayMap = {}
