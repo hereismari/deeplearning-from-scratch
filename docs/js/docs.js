eb954bfd0e6436554f6195a84712224846e77dc7
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2018-09-10 15:42:24 -0300

    Finished neural nets

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
-var analysisArray = ['pca', 'neural-net']
+var analysisArray = ['pca', 'intro-neural-nets', 'neural-net']
 var analysisDict = {'Depend-ncias-1': 'pca'}
 var analysisArrayMap = {'intro-neural-nets': 'Dependências', 'neural-net': 'Dependências', 'pca': 'Dependências'}
